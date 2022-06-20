0ebfe181fb226387002127b06e848a4eff7b70a3
NishatTonni <39293551+NishatTonni@users.noreply.github.com>
2022-06-20 18:46:24 +0600

    Isotop

---
+++ Isotop/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.grid').isotope({
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        masonry: {
+          // use outer width of grid-sizer for columnWidth
+          columnWidth: '.grid-sizer'
+        }
+      })
+})
